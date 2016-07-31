6c21ff9fb250c1b0a1f81c53f4c3826a07889632
Samantha Musselman <samantha.musselman212@gmail.com>
2016-07-30 21:58:08 -0500

    Pie chart

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawBarChart);
+google.charts.setOnLoadCallback(drawPieChart);
 
 function drawBarChart() {
     var chartOneData = google.visualization.arrayToDataTable([
@@ -35,8 +36,41 @@ function drawBarChart() {
     var chartOne = new google.visualization.BarChart(document.getElementById('chartOne'));
 
     chartOne.draw(chartOneData, chartOneOptions);
+
 };
 
+function drawPieChart() {
+
+    var pieChartData = google.visualization.arrayToDataTable([
+        ['Country', 'Number of Participants'],
+        ['Venezuela', 1],
+        ['Argentina', 3],
+        ['Russia', 2],
+        ['China', 4],
+        ['Bulgaria', 1],
+        ['United Kingdom', 1],
+        ['United States', 2],
+        ['Canada', 1],
+        ['Mexico', 3],
+        ['Tanzania', 1],
+        ['Swaziland', 1],
+        ['India', 3],
+        ['Austria', 1],
+        ['Poland', 1]
+    ]);
+
+    var pieChartOptions = {
+        title: "Participants' Home Countries",
+        chartArea: {
+            width: 500,
+            height: 400
+        }
+    };
+
+    var pieChart = new google.visualization.PieChart(document.getElementById('chartTwo'));
+
+    pieChart.draw(pieChartData, pieChartOptions);
+}
 
 // Sample Data for Later
 //var countries = {
